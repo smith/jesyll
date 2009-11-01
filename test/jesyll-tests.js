885b674502162dacd8c0742be51a9953dae6a860
Andy Chu <andy@chubot.org>
2009-11-01 01:43:29 -0400

    Parsing metadata

--- test/jesyll-tests.js
+++ test/jesyll-tests.js
@@ -5,5 +5,11 @@ exports.testParseSourceDoc = function() {
     assert.isEqual("foo", jesyll.parseSourceDoc("foo"));
 }
 
+exports.testParseMetadata = function() {
+    assert.eq(
+        {title: "Title", layout: "Layout"},
+        jesyll.parseMetadata(["  title: Title \n", " layout: Layout \n"]));
+}
+
 if (require.main === module.id)
     require("test/runner").run(exports);
