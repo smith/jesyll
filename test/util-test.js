af6afb7d845ba8ceaf362d7cc0e606ab69db1a94
Andy Chu <andy@chubot.org>
2009-11-22 14:40:23 -0500

    Get rid of Composite / ObjComposite

--- test/util-test.js
+++ test/util-test.js
@@ -3,25 +3,6 @@
 var assert = require("test/assert"),
     util = require("jesyll/util");
 
-exports.testComposite = function() {
-    var defaults = {a: 0, b: 0, c: 0};
-    var config = {a: 100, b: 200};
-    var flags = {a: 3, c: 5};
-
-    var c = util.ObjComposite(defaults, config, flags);
-
-    print("c: " + c.get('a') + " " + c.get('b') + " " + c.get('c'));
-    assert.isEqual(c.get('a'), 3);
-    assert.isEqual(c.get('b'), 200);
-    assert.isEqual(c.get('c'), 5);
-
-    var flags2 = {};
-    var c = new util.ObjComposite(defaults, config, flags2);
-    assert.isEqual(c.get('a'), 100);
-    assert.isEqual(c.get('b'), 200);
-    assert.isEqual(c.get('c'), 0);
-}
-
 exports.testVarStack = function() {
     var defaults = {a: 0, b: 0, c: 0};
     var config = {a: 100, b: 200};
