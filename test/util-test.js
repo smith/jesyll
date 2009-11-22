e44234000f8060fe88c3e51aaca36da965218f0d
Andy Chu <andy@chubot.org>
2009-11-22 01:45:35 -0500

    Initial cut of ObjectStack (may need better name)

--- test/util-test.js
+++ test/util-test.js
@@ -22,5 +22,25 @@ exports.testComposite = function() {
     assert.isEqual(c.get('c'), 0);
 }
 
+exports.testObjectStack = function() {
+    var defaults = {a: 0, b: 0, c: 0};
+    var config = {a: 100, b: 200};
+    var flags = {a: 3, c: 5};
+
+    var c = util.ObjectStack(defaults, config, flags);
+
+    print("c: " + c.get('a') + " " + c.get('b') + " " + c.get('c'));
+    assert.isEqual(c.get('a'), 3);
+    assert.isEqual(c.get('b'), 200);
+    assert.isEqual(c.get('c'), 5);
+
+    var flags2 = {};
+    var c = new util.ObjComposite(defaults, config, flags2);
+    assert.isEqual(c.get('a'), 100);
+    assert.isEqual(c.get('b'), 200);
+    assert.isEqual(c.get('c'), 0);
+}
+
+
 if (require.main === module.id)
     require("test/runner").run(exports);
