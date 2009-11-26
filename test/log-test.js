d51b790b5f4114fa7ba388daec900438a8859ec6
Andy Chu <andy@chubot.org>
2009-11-26 03:05:27 -0500

    Implement the --verbose flag for the app, and update the log module.

--- test/log-test.js
+++ test/log-test.js
@@ -6,16 +6,20 @@ var assert = require("test/assert"),
 exports.testLogger = function() {
     var logger = log.Logger(),
         name = "world";
+    logger.debug('debug %s', name);
     logger.info('hello %s', name);
     logger.warning('warning %s', name);
     logger.error('error %s', name);
+    logger.fatal('fatal %s', name);
 }
 
 exports.testStatics = function() {
     var name = "world";
+    log.debug('debug %s', name);
     log.info('hello %s', name);
     log.warning('warning %s', name);
     log.error('error %s', name);
+    log.fatal('fatal %s', name);
 }
 
 if (require.main === module.id)
