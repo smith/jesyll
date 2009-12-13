86056475414cb21afc08c3110f2e92ebf612c016
Andy Chu <andy@chubot.org>
2009-12-13 13:43:49 -0500

    A more CommonJS-like way of reading plugins.  Still need to sandbox the execution of the plugin.

--- test/plugins-test.js
+++ test/plugins-test.js
@@ -6,7 +6,29 @@ var log = require("jesyll/log"),
     plugins = require("jesyll/plugins");
 
 exports.testReadPlugins = function() {
-  log.info('TODO');
+  var fs = {
+    contentsOf: function () {
+      return "PLUGINS.onDocPush = function() { return {a: 3} };";
+    }
+  }
+  var p = plugins.readPlugins(fs);
+  var result = p.onDocPush();
+  assert.eq(3, result.a);
+}
+
+exports.testPluginsDontLeakGlobals = function() {
+  return;  // TODO: Fix
+  var fs = {
+    contentsOf: function () {
+      // This tries to introduce a new global, but the plugin system disallows
+      // it with hermetic eval
+      return "myGlobal = 3; " +
+             "PLUGINS.onDocPush = function() { return {a: 3} };";
+    }
+  }
+  var p = plugins.readPlugins(fs);
+  var result = p.onDocPush();
+  assert.eq(3, result.a);
 }
 
 if (require.main === module.id)
